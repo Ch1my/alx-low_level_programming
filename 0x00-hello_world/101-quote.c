a5caf6fc281e1f443cccff00f6902b8be123c585
Emmanuel <emmanuelazubuko@yahoo.com>
2022-10-14 20:43:05 -0700

    done with the last advanced project now

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 
 /**
  * main - Entry point
- * 
+ *
  *Return: 1
  */
 int main(void)
 {
 	char *mgs =  "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDOUT_FILENO, mgs, 59);
+		write(STDOUT_FILENO, mgs, 59);
 	return (1);
 }
