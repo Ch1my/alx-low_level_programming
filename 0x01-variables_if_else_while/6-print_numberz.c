5cf67ff7bef4fc30c79d15ef5e61cb8461f6dd93
Emmanuel <emmanuelazubuko@yahoo.com>
2022-10-17 20:30:01 -0700

    variables project 1 to 5

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	int num;
 
-        for (num = 0; num <= 9; num++)
-        {
-                putchar(num);
-        }
-        putchar('\n');
-        return (0);
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
 }
